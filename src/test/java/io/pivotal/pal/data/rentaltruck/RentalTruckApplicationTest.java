e87df5fbf4bd86530a28052c0d0dfe4f9a319692
David Wadden <dwadden@pivotal.io>
2018-03-29 23:21:48 -0500

    Lints code and renames to RentalTruckApp

--- src/test/java/io/pivotal/pal/data/rentaltruck/RentalTruckApplicationTest.java
+++ src/test/java/io/pivotal/pal/data/rentaltruck/RentalTruckApplicationTest.java
@@ -1,4 +1,4 @@
-package io.pivotal.pal.data.fakesource;
+package io.pivotal.pal.data.rentaltruck;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FakeSourceApplicationTests {
+public class RentalTruckApplicationTest {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
