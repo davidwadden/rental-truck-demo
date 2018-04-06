c5a2be82850da96730b44ba11853fc18d60cda8e
David Wadden <dwadden@pivotal.io>
2018-04-06 01:12:52 -0500

    Finds dropoff stores by location from Cassandra

---
+++ reservation/src/main/java/io/pivotal/pal/data/rentaltruck/reservation/dropoffstore/query/StoreByLocationQueryController.java
@@ -0,0 +1,23 @@
+package io.pivotal.pal.data.rentaltruck.reservation.dropoffstore.query;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+public class StoreByLocationQueryController {
+
+    private final StoreByLocationQueryRepository repository;
+
+    public StoreByLocationQueryController(StoreByLocationQueryRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/stores-by-location")
+    public Collection<StoreByLocationProjection> listStoresByLocation(@RequestParam String city,
+                                                                      @RequestParam String state) {
+        return repository.findAllProjectedByLocation(city, state);
+    }
+}
