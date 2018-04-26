68b8f5ba6520ddea57ace878181e1e9d42ca5491
tdalsing-pivotal <tdalsing@pivotal.io>
2018-04-26 12:41:50 -0500

    adds retry to sync events

--- framework/src/main/java/io/pivotal/pal/data/framework/event/SyncEventSubscriberAdapter.java
+++ framework/src/main/java/io/pivotal/pal/data/framework/event/SyncEventSubscriberAdapter.java
@@ -4,15 +4,18 @@ public class SyncEventSubscriberAdapter<C, R> extends SyncEventChannel {
 
     private SyncEventHandler<C, R> handler;
     private SyncEventHandler<C, R> errorHandler;
+    private int maxRetryCount;
+    private long initialRetryWaitTime;
+    private int retryWaitTimeMultiplier;
 
-    public SyncEventSubscriberAdapter(String eventName, SyncEventHandler<C, R> handler) {
-        this(eventName, handler, null);
-    }
-
-    public SyncEventSubscriberAdapter(String eventName, SyncEventHandler<C, R> handler, SyncEventHandler<C, R> errorHandler) {
+    public SyncEventSubscriberAdapter(String eventName, SyncEventHandler<C, R> handler, SyncEventHandler<C, R> errorHandler,
+                                      int maxRetryCount, long initialRetryWaitTime, int retryWaitTimeMultiplier) {
         super(eventName);
         this.handler = handler;
         this.errorHandler = errorHandler;
+        this.maxRetryCount = maxRetryCount;
+        this.initialRetryWaitTime = initialRetryWaitTime;
+        this.retryWaitTimeMultiplier = retryWaitTimeMultiplier;
 
         //noinspection unchecked
         registerSubscriber((SyncEventSubscriberAdapter<Object, Object>) this);
@@ -20,7 +23,24 @@ public class SyncEventSubscriberAdapter<C, R> extends SyncEventChannel {
 
     public R onEvent(C data) {
         try {
-            return handler.onEvent(data);
+            long waitTime = initialRetryWaitTime;
+
+            for (int i = 0; i <= maxRetryCount; ++i) {
+                try {
+                    return handler.onEvent(data);
+                } catch (Exception t) {
+                    if (i < maxRetryCount) {
+                        synchronized (this) {
+                            this.wait(waitTime);
+                        }
+                        waitTime *= retryWaitTimeMultiplier;
+                    } else {
+                        throw t;
+                    }
+                }
+            }
+
+            return null;
         } catch (Exception e) {
             if (errorHandler == null) {
                 return null;
--- framework/src/test/java/io/pivotal/pal/data/framework/event/SyncEventTest.java
+++ framework/src/test/java/io/pivotal/pal/data/framework/event/SyncEventTest.java
@@ -9,10 +9,10 @@ public class SyncEventTest {
     private static final String EVENT_NAME = "test";
 
     @Test
-    public void success() {
+    public void success_withoutRetry() {
         DefaultSyncEventPublisher<String, String> publisher = new DefaultSyncEventPublisher<>(EVENT_NAME);
         Handler handler = new Handler();
-        SyncEventSubscriberAdapter<String, String> subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler);
+        SyncEventSubscriberAdapter<String, String> subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler, null, 0, 0, 0);
 
         handler.setData(null);
 
@@ -24,11 +24,26 @@ public class SyncEventTest {
     }
 
     @Test
-    public void error_withHandler() {
+    public void success_withRetry() {
         DefaultSyncEventPublisher<String, String> publisher = new DefaultSyncEventPublisher<>(EVENT_NAME);
-        ExceptionThrowingHandler handler = new ExceptionThrowingHandler();
+        ExceptionThrowingHandler handler = new ExceptionThrowingHandler(1);
+        SyncEventSubscriberAdapter<String, String> subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler, null, 1, 100, 2);
+
+        handler.setData(null);
+
+        String someData = "some-data";
+        String result = publisher.publish(someData);
+
+        assertThat(handler.getData()).isEqualTo(someData);
+        assertThat(result).isEqualTo(someData);
+    }
+
+    @Test
+    public void error_withHandlerNoRetry() {
+        DefaultSyncEventPublisher<String, String> publisher = new DefaultSyncEventPublisher<>(EVENT_NAME);
+        ExceptionThrowingHandler handler = new ExceptionThrowingHandler(1);
         Handler errorHandler = new Handler();
-        SyncEventSubscriberAdapter<String, String> subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler, errorHandler);
+        SyncEventSubscriberAdapter<String, String> subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler, errorHandler, 0, 0, 0);
 
         String someData = "some-data";
         String result = publisher.publish(someData);
@@ -38,10 +53,10 @@ public class SyncEventTest {
     }
 
     @Test
-    public void error_withoutHandler() {
+    public void error_withoutHandlerNoRetry() {
         DefaultSyncEventPublisher<String, String> publisher = new DefaultSyncEventPublisher<>(EVENT_NAME);
-        ExceptionThrowingHandler handler = new ExceptionThrowingHandler();
-        SyncEventSubscriberAdapter<String, String> subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler);
+        ExceptionThrowingHandler handler = new ExceptionThrowingHandler(1);
+        SyncEventSubscriberAdapter<String, String> subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler, null, 0, 0, 0);
 
         String someData = "some-data";
         String result = publisher.publish(someData);
@@ -51,9 +66,30 @@ public class SyncEventTest {
 
     private class ExceptionThrowingHandler implements SyncEventHandler<String, String> {
 
+        int maxCount;
+        int count = 0;
+        String data;
+
+        public ExceptionThrowingHandler(int maxCount) {
+            this.maxCount = maxCount;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
         @Override
         public String onEvent(String data) {
-            throw new IllegalArgumentException("data = "+data);
+            if (count++ < maxCount) {
+                throw new IllegalArgumentException("data = " + data);
+            } else {
+                this.data = data;
+                return data;
+            }
         }
     }
 
