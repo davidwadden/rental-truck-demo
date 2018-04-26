d31b15a4bd326dada015b794add14e0701a22c04
tdalsing-pivotal <tdalsing@pivotal.io>
2018-04-26 11:07:49 -0500

    adds error handling to sync events

--- framework/src/main/java/io/pivotal/pal/data/framework/event/SyncEventSubscriberAdapter.java
+++ framework/src/main/java/io/pivotal/pal/data/framework/event/SyncEventSubscriberAdapter.java
@@ -3,16 +3,30 @@ package io.pivotal.pal.data.framework.event;
 public class SyncEventSubscriberAdapter<C, R> extends SyncEventChannel {
 
     private SyncEventHandler<C, R> handler;
+    private SyncEventHandler<C, R> errorHandler;
 
-    public SyncEventSubscriberAdapter(String eventName, SyncEventHandler<C, R> subscriber) {
+    public SyncEventSubscriberAdapter(String eventName, SyncEventHandler<C, R> handler) {
+        this(eventName, handler, null);
+    }
+
+    public SyncEventSubscriberAdapter(String eventName, SyncEventHandler<C, R> handler, SyncEventHandler<C, R> errorHandler) {
         super(eventName);
-        this.handler = subscriber;
+        this.handler = handler;
+        this.errorHandler = errorHandler;
 
         //noinspection unchecked
         registerSubscriber((SyncEventSubscriberAdapter<Object, Object>) this);
     }
 
     public R onEvent(C data) {
-        return handler.onEvent(data);
+        try {
+            return handler.onEvent(data);
+        } catch (Exception e) {
+            if (errorHandler == null) {
+                return null;
+            } else {
+                return errorHandler.onEvent(data);
+            }
+        }
     }
 }
--- framework/src/test/java/io/pivotal/pal/data/framework/event/SyncEventTest.java
+++ framework/src/test/java/io/pivotal/pal/data/framework/event/SyncEventTest.java
@@ -1,6 +1,5 @@
 package io.pivotal.pal.data.framework.event;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,26 +8,53 @@ public class SyncEventTest {
 
     private static final String EVENT_NAME = "test";
 
-    private DefaultSyncEventPublisher<String, String> publisher;
-    private Handler handler;
-    private SyncEventSubscriberAdapter<String, String> subscriber;
-
-    @Before
-    public void setUp() {
-        publisher = new DefaultSyncEventPublisher<>(EVENT_NAME);
-        handler = new Handler();
-        subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler);
+    @Test
+    public void success() {
+        DefaultSyncEventPublisher<String, String> publisher = new DefaultSyncEventPublisher<>(EVENT_NAME);
+        Handler handler = new Handler();
+        SyncEventSubscriberAdapter<String, String> subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler);
 
         handler.setData(null);
-    }
 
-    @Test
-    public void basicTest() {
         String someData = "some-data";
         String result = publisher.publish(someData);
 
         assertThat(handler.getData()).isEqualTo(someData);
         assertThat(result).isEqualTo(someData);
+    }
+
+    @Test
+    public void error_withHandler() {
+        DefaultSyncEventPublisher<String, String> publisher = new DefaultSyncEventPublisher<>(EVENT_NAME);
+        ExceptionThrowingHandler handler = new ExceptionThrowingHandler();
+        Handler errorHandler = new Handler();
+        SyncEventSubscriberAdapter<String, String> subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler, errorHandler);
+
+        String someData = "some-data";
+        String result = publisher.publish(someData);
+
+        assertThat(errorHandler.getData()).isEqualTo(someData);
+        assertThat(result).isEqualTo(someData);
+    }
+
+    @Test
+    public void error_withoutHandler() {
+        DefaultSyncEventPublisher<String, String> publisher = new DefaultSyncEventPublisher<>(EVENT_NAME);
+        ExceptionThrowingHandler handler = new ExceptionThrowingHandler();
+        SyncEventSubscriberAdapter<String, String> subscriber = new SyncEventSubscriberAdapter<>(EVENT_NAME, handler);
+
+        String someData = "some-data";
+        String result = publisher.publish(someData);
+
+        assertThat(result).isNull();
+    }
+
+    private class ExceptionThrowingHandler implements SyncEventHandler<String, String> {
+
+        @Override
+        public String onEvent(String data) {
+            throw new IllegalArgumentException("data = "+data);
+        }
     }
 
     private class Handler implements SyncEventHandler<String, String> {
